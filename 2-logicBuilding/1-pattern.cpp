977dcf6a41c61082b95ed24b0eb4179709efdb7c
shikharuttam16 <90438036+shikharuttam16@users.noreply.github.com>
2025-02-15 03:24:51 +0000

    pattern questions are added

--- 2-logicBuilding/1-pattern.cpp
+++ 2-logicBuilding/1-pattern.cpp
@@ -10,9 +10,63 @@ void print1(int n){
         cout<<endl;
     }
 }
+
+void print2(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void print3(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=1;j<=i+1;j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void print4(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j=0;j<i;j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+// *****
+// ****
+// ***
+// **
+// *
+// i = 0 j = 5 
+// i = 1 j = 4
+// i = 2 j = 3
+void print5(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=n-i ;j>0;j--){
+            cout << "*";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
    int n;
    cout << "Enter number";
    cin >> n;
-   print1(n);
+//    print1(n);
+//    print2(n);
+//    print3(n);
+//    print4(n);
+      print5(n);
 }
+
