c49fdf68a486f39f5abf8a10747fd6f86170f416
Shikhar Uttma <uttam16shikhar@gmail.com>
2025-04-07 04:22:06 +0530

    code push

--- 1-basics/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    // string s1,s2;
-    // cout << "Enter full name";
-    // cin >> s1 >> s2;
-    // cout << s1 << " " << s2;
---
+++ array/Basics.java
@@ -0,0 +1,12 @@
+class Basics {
+
+    public static void main(String[] args){
+        int rollNo[] = new int[5];
+        int dummy[] = {
+            1,2,3,4,5
+        };
+        for (int i = 0; i < rollNo.length; i++) {
+            System.out.print(rollNo[i]+" -> ");
+        }
+    }
+}
